3ddeb0c566651c22b75de2a4ca0dc283afb76d1c
Ryusei-M-G <ryusei.morishima1023@gmail.com>
2025-10-05 01:08:49 +0900

    MUIの導入

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,17 @@
-function App() {
+import { Button, Typography, Container, Box } from '@mui/material'
 
+function App() {
   return (
-    <>
-    test
-    </>
+    <Container>
+      <Box sx={{ my: 4 }}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          MUI最小構成
+        </Typography>
+        <Button variant="outlined" color="primary">
+          サンプルボタン
+        </Button>
+      </Box>
+    </Container>
   )
 }
 

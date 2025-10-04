f2050b001e9e8a0abf2b24698d8246317aea01c9
Ryusei-M-G <ryusei.morishima1023@gmail.com>
2025-10-05 01:13:01 +0900

    AppコンポーネントのBoxにスタイルを追加し、セクションとしての役割を明確化

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import { Button, Typography, Container, Box } from '@mui/material'
 function App() {
   return (
     <Container>
-      <Box sx={{ my: 4 }}>
+      <Box component="section" sx={{ p: 2, border: '1px dashed grey' }}>
         <Typography variant="h4" component="h1" gutterBottom>
           MUI最小構成
         </Typography>

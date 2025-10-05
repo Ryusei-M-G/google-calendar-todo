9b76ecbb3744735d9ec53d1becf0317d4a5608bb
Ryusei-M-G <ryusei.morishima1023@gmail.com>
2025-10-05 13:02:32 +0900

    Navigaterコンポーネントを新規作成し、MUIを使用して基本的なレイアウトを追加

---
+++ client/src/components/Navigater.tsx
@@ -0,0 +1,12 @@
+import { Box, Container, Button } from "@mui/material";
+const Navigater = () => {
+  return (
+    <Container>
+      <Box>
+        <Button variant="outlined">Home</Button>
+      </Box>
+    </Container>
+  );
+};
+
+export default Navigater;

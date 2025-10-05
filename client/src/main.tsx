9c853a4d2dd6b051b42ee6bd14c121a2416b7e4c
Ryusei-M-G <ryusei.morishima1023@gmail.com>
2025-10-05 13:03:34 +0900

    react-router-domを依存関係に追加

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { CssBaseline } from '@mui/material'
 import App from './App.tsx'
+import Navigater from './components/Navigater.tsx'
+import { }
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>

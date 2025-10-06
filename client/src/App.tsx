decc4c532cced539dd3e208a6966457014a8f410
Ryusei-M-G <ryusei.morishima1023@gmail.com>
2025-10-06 11:43:56 +0900

    feat: add login handler to the login button in App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,18 @@
 import { Button, Typography, Container, Box } from '@mui/material'
 
 function App() {
+
+  const loginHandle = () =>{
+    console.log('ログイン（モック）');
+    // window.location.href = 'http://localhost:3000/auth';
+  }
   return (
     <Container style={{textAlign:'center'}} >
       <Box component="section" sx={{ p: 2, border: '1px dashed grey' }}>
         <Typography variant="h4" component="h1" gutterBottom>
           google calendar todo
         </Typography>
-        <Button variant="contained" color="primary">
+        <Button variant="contained" color="primary" onClick={loginHandle}>
           googleでログイン
         </Button>
       </Box>

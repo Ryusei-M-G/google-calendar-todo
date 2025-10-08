ad0dd6bb646bdbf15bff1e41f585c6626a882326
Ryusei-M-G <ryusei.morishima1023@gmail.com>
2025-10-08 13:30:50 +0900

    エンドポイントの関数整備

---
+++ server/google_oauth.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from 'express';
+
+const google_oauth = async (req: Request, res: Response): Promise<void> => {
+
+}
+
+export default google_oauth;
--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,13 @@
 import express from 'express'
 
+import google_oauth from './google_oauth';
+import callback from './callback';
 const app = express();
 
 const port = 3000;
+
+app.get('/auth',google_oauth);
+app.get('/callback',callback);
 app.listen(port, ()=>{
   console.log('server is running')
 })
